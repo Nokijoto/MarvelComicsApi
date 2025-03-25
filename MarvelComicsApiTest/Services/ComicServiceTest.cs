8e372916aad0220c4e69db8eacfbd236309f87a7
Nokijoto <nokijoto@protonmail.com>
2025-03-25 15:12:41 +0100

    added simple test for service

---
+++ MarvelComicsApiTest/Services/ComicServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using MarvelComicsApi.Interfaces;
+using MarvelComicsApi.Mappers;
+using MarvelComicsApi.Repositories;
+using MarvelComicsApi.Services;
+using MarvelComicsApiTest.Factories;
+using Microsoft.Extensions.Logging;
+using Moq;
+namespace MarvelComicsApiTest.Services;
+
+public class ComicServiceTest
+{
+    private readonly IComicService _comicService;
+    private readonly Mock<IComicRepository> _mockedComicRepository;
+    private readonly ComicFactory _comicFactory;
+    public ComicServiceTest()
+    {
+        _comicFactory = new ComicFactory();
+        _mockedComicRepository= new Mock<IComicRepository>();
+        _comicService = new ComicService(_mockedComicRepository.Object);
+    }
+
+    [Fact]
+    public async Task GetComicAsyncTest()
+    {        
+        var comic =_comicFactory.Create();
+
+        _mockedComicRepository.Setup(repo => repo.GetComic()).ReturnsAsync(comic.ToDto());
+        
+        var result = await _comicService.GetComic();
+        
+        
+        
+        Assert.NotNull(comic);        
+    }
+}
