c79989cb97afcb5e816fba12ebb5b4dd205b7b7e
Nokijoto <nokijoto@protonmail.com>
2025-03-27 18:59:43 +0100

    add new controller for tests

---
+++ MarvelComicsApi/Controllers/VersionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarvelComicsApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class VersionController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "1.0.0";
+    }
+}
